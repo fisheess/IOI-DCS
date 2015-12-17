d00727a55d5f768816200a9e5ba3fe02d13297cd
Jingrui <yu.jingrui.cn@gmail.com>
2015-12-17 16:07:12 +0100

    dlmFitting.m is the main program. timeCourses.mat contains curves from the 11 data sets.

---
+++ dlmFitting.m
@@ -0,0 +1,27 @@
+
+path='D:\Dropbox\DA\Matlab\testdata_processed_figures\';
+% load data
+load([path 'timeCourses.mat']);
+
+t = tc{1}(:,1);
+y = tc{1}(:,2); 
+
+% state and observation matrix
+opt = struct('trig',2,'order',1);
+[G,F] = dlmgensys(opt);
+
+% scale y for numerical stability
+ys = stdnan(y);
+yy = y./ys;
+
+%
+ym = meannan(yy); % mean observations
+wtrend = abs(ym)*0.005/12; % trend std
+wseas = 0.01;% seasonal component std
+w0 = [0 wtrend wseas wseas wseas wseas];
+
+%
+opt = struct('trig',2,'mcmc',1,'nsimu',2000);
+opt.winds = [0 1 2 2 2 2];
+opt.varcv = [1 1];
+dlm = dlmfit(yy,122,w0,[],[],[],opt);
